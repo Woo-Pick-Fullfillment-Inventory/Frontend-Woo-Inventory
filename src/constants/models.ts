26c76c554bbc0a5047e64557a42200a2f3331fb7
khaihoang-shopping <151777859+khaihoang-shopping@users.noreply.github.com>
2024-02-21 16:41:13 +0100

    Update src/constants/models.ts
    Co-authored-by: Giang Pham <44295841+GiangPhammm@users.noreply.github.com>

--- src/constants/models.ts
+++ src/constants/models.ts
@@ -1,7 +1,7 @@
+// Include other fields that your error response might have
 interface ApiValidationErrorResponse {
   type: string;
   message: string;
-  // Include other fields that your error response might have
 }
 
 export const isApiValidationErrorResponse = (result: unknown): result is ApiValidationErrorResponse => result!== undefined;
