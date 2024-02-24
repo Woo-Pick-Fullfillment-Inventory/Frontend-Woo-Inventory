f397b28ab446dd8700d62e6a451ebc96f3ba0052
khaihoang-shopping <khaihoang-shopping@gmail.com>
2024-02-24 19:02:31 +0100

    use ES6 import

--- metro.config.js
+++ metro.config.js
@@ -1,18 +1,13 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
-
-/**
- * Metro configuration
- * https://facebook.github.io/metro/docs/configuration
- *
- * @type {import('metro-config').MetroConfig}
- */
+// Correct usage of dynamic import within an async function
 async function getConfig() {
-    const defaultConfig = await getDefaultConfig(__dirname);
-    const config = {
-        // Your custom configuration here
-    };
+  const { getDefaultConfig, mergeConfig } = await import('@react-native/metro-config');
 
-    return mergeConfig(defaultConfig, config);
+  const defaultConfig = await getDefaultConfig(__dirname);
+  const config = {
+      // Your custom configuration here
+  };
+
+  return mergeConfig(defaultConfig, config);
 }
 
 module.exports = getConfig();
